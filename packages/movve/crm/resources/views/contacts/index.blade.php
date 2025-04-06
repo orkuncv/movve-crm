e88b77165a07af520cd89b565b83787acb4020e4
Orkun <ocavdar@outlook.com>
2025-04-07 00:09:10 +0200

    Added lang

--- packages/movve/crm/resources/views/contacts/index.blade.php
+++ packages/movve/crm/resources/views/contacts/index.blade.php
@@ -126,13 +126,6 @@
                                                 </svg>
                                                 {{ __('crm::crm.view') }}
                                             </a>
-                                            <a href="/{{ app()->getLocale() }}/crm/contacts/{{ $contact->id }}/edit"
-                                               class="inline-flex items-center px-3 py-1.5 bg-gray-600 border border-transparent rounded-md text-xs text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 transition-colors duration-150 shadow-sm">
-                                                <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                                </svg>
-                                                {{ __('crm::crm.edit') }}
-                                            </a>
                                         </td>
                                     </tr>
                                 @empty
